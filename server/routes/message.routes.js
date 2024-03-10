0ecd3ad2947c17b5eb93cb0e961d73be53ef953c
Irainelly Alvarez <irainelly29@gmail.com>
2024-03-10 18:58:53 -0300

    router is changed

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createMessage, getAllMessages, getMessageById } from '../controllers/message.controllers.js';
+import { createMessage, deleteMessage, getAllMessages, getMessageById, updateMessage } from '../controllers/message.controllers.js';
 
 const router = Router();
 
 router.get('/', getAllMessages);
 router.get('/:messageId', getMessageById);
 router.post('/', createMessage);
-router.put('/:messageId', createMessage);
+router.put('/:messageId', updateMessage);
+router.deleted('/:messageId', deleteMessage);
 
 export default router;
