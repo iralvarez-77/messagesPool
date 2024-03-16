2a2221c6e004db8507f6cbd010ff9a7e281dea7a
Irainelly Alvarez <irainelly29@gmail.com>
2024-03-16 01:08:04 -0300

    messageRoutes to v1messageRoute

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
-import messageRoutes from './routes/message.routes.js';
+import v1messageRoute from './routes/message.routes.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use('/api/v1/messages', messageRoutes);
+app.use('/api/v1/messages', v1messageRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
